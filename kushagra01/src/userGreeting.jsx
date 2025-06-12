f621d87d29b0b7455d85c9fd3e411659f5d3b3b9
kushagrabisht05 <kushagrabisht10@gmail.com>
2025-06-12 22:39:34 +0530

    some updates

--- kushagra01/src/userGreeting.jsx
+++ kushagra01/src/userGreeting.jsx
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
 
-function userGreeting(props){
-    const greetMsg = <h1 className="greet">Greetings from the server! Welcome {props.user}</h1>
-    const errorPrompt = <h1 className="error">Please log in first to continue</h1>
+function UserGreeting(props) {
+  const greetMsg = <h1 className="greet">Greetings from the server! Welcome {props.user}</h1>;
+  const errorPrompt = <h1 className="error">Please log in first to continue</h1>;
 
-    return(props.isLoggedIn ? greetMsg : errorPrompt 
-    );
+  return props.isLoggedIn ? greetMsg : errorPrompt;
 }
 
-userGreeting.PropTypes={
-    isLoggedIn : PropTypes.bool,
-    user : PropTypes.string
-}
-export default userGreeting
+UserGreeting.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  user: PropTypes.string,
+};
+
+export default UserGreeting;
