21c8003dfcf66ba97e6611ed89ceb61f5e08a066
kushagrabisht05 <kushagrabisht10@gmail.com>
2025-06-13 14:33:21 +0530

    changes in list rendering using map and filter functions

--- kushagra01/src/List.jsx
+++ kushagra01/src/List.jsx
@@ -1,9 +1,21 @@
-function List(){
-    const fruits=["Apple","Banana","Strawberry","Mango"];
-    const listItems = fruits.map(fruit=><li>{fruit}</li>);
+function List(Props){
+    // const fruits=["Apple","Banana","Strawberry","Mango"];
+    // now making each fruit an object of its own
+
+    const fruits=[{id:1,name:"Apple", cals:150},
+                {id:2,name:"Banana", cals:100},
+                {id:3,name:"Strawberry", cals:75},
+                {id:4,name:"Mango", cals:200}
+    ];
+
+    const lowCalFruits=fruits.filter(fruit=> fruit.cals<150);
+    //to display this, replace fruits with lowCalFruits in listItems and same for fruit element
+
+    const listItems = fruits.map(fruit=> <li key={fruit.id}> {fruit.name}: {fruit.cals}</li>);
+    //react prefers a unique key item for each element
 
     return(
-        <ol>{listItems}</ol>
+        <ul>{listItems}</ul>
     );
 }
 
