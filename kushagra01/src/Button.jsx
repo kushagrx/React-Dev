af02cfa66ea26383fb4e5b092a8b2f00e682d8d6
kushagrabisht05 <kushagrabisht10@gmail.com>
2025-06-13 21:54:33 +0530

    basic onclick event

--- kushagra01/src/Button.jsx
+++ kushagra01/src/Button.jsx
@@ -1,4 +1,7 @@
 function Button(){
-    return(<button className="button">Click me</button>);
+
+    const handleClick=(event)=>event.target.textContent = "hello";
+
+    return(<button onClick={(event)=>handleClick(event)} className="button">Click me</button>);
 }
 export default Button
