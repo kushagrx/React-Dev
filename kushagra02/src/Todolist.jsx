c5d2fa4dc2bb52fce0de4796baa49e643fde217d
kushagrx <kushagrabisht10@gmail.com>
2025-07-15 18:04:33 +0530

    Added move task up and down buttons

--- kushagra02/src/Todolist.jsx
+++ kushagra02/src/Todolist.jsx
@@ -1,7 +1,7 @@
 import React,{useState} from "react";
 
 function Todolist(){
-    const [tasks,setTasks]=useState([]);
+    const [tasks,setTasks]=useState(["Take a shower","Eat breakfast","Do coding"]);
     const [newtask,setNewtask]=useState("");
 
     function handleInputChange(event){
@@ -28,6 +28,21 @@ function Todolist(){
                 <input type="text" placeholder="Enter a task" value={newtask} onChange={handleInputChange}/>
                 <button className="addbutton" onClick={addTask}>Add Task</button>
             </div>
+
+            <ol>
+                {tasks.map((task, index) => (
+                    <li key={index}>
+                        <span className="text">{task}</span>
+                        <button className="deletebutton" onClick={() => deleteTask(index)}>
+                        Delete</button>
+                        <button className="moveupbutton" onClick={() => moveTaskUp(index)}>
+                        ☝️</button>
+                        <button className="movedownbutton" onClick={() => moveTaskDown(index)}>
+                        👇</button>
+                    </li>
+                ))}
+            </ol>
+
         </div>
 
     )
